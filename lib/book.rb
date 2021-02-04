e965df82ee8b6232aacbef05f4dd1ec0b0913505
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-04 20:30:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,17 @@
 class Book
 
-attr_accessor :author, :page_count, :genre
+  attr_accessor :title, :author, :page_count, :genre
 
-  def initialize(title)
-    @title = title
-  end
+def initialize(title)
+  @title = title
+  @author = author
+  @page_count = page_count
+  @genre = genre
+end
 
-  def title
-    @title
-  end
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+
 end
